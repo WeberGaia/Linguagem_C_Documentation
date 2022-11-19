49ef57b9a406d932fd7b22ecac0a4e0b569bc271
Weber Gaia <webergaia@id.uff.br>
2022-11-18 21:29:34 -0300

    Jogo de adivinhação

--- codificacao.c
+++ codificacao.c
@@ -4,11 +4,11 @@
 
 int main ()
 {
-    // Bloco que configura o idioma e plota o cabeçalho
+    /* Bloco que configura o idioma e plota o cabeçalho
     setlocale(LC_ALL,"Portuguese_brasil"); // Rodar no terminal o seguinte código -> chcp 65001
-    printf("=====================================================\n");
-    printf("Olá Weber! Bem-vindo ao jogo de adivinhação da Alura.\n");
-    printf("=====================================================\n");
+    printf("==============================================\n");
+    printf("Olá! Bem-vindo ao jogo de adivinhação da Alura.\n");
+    printf("==============================================\n");
 
     int tentativas = 5;
     for(int i=1; i<=tentativas;i++){
@@ -34,5 +34,38 @@ int main ()
             else{
                 printf("O número escolhido é menor que o número secreto\n");
             }   
-        }       
+        }*/
+
+    printf("===============================================\n");
+    printf("Olá! Bem-vindo ao jogo de adivinhação da Alura.\n");
+    printf("===============================================\n");
+
+    int tentativas = 1;
+    while(1)
+    {
+        printf("Tentativa %d\n",tentativas);
+        int num_secreto = 90;
+        int chute;
+        printf("Digite o chute: ");
+        scanf("%d", &chute);
+
+        //Estrutura de condições 
+        int acertou = chute == num_secreto;
+        int maior = chute > num_secreto;
+        int menor = chute < num_secreto;
+
+        if (acertou)
+        {
+            printf("Parabéns! Você acertou.\n");
+            break;
+        }
+        else if(maior){
+                printf("O número escolhido é maior que o número secreto\n");
+            }
+            else{
+                printf("O número escolhido é menor que o número secreto\n");
+            }   
+        tentativas++;
+    }       
+    printf("Você tentou %d vezes até acertar o número",tentativas);
 }
