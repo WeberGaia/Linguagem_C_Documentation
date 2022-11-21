32547374b016688969fe52016ea857fbc7c037e1
Weber Gaia <webergaia@id.uff.br>
2022-11-21 20:11:41 -0300

    21/11/2022

--- Documentacao.c
+++ Documentacao.c
@@ -15,4 +15,12 @@ LINGUAGEM DE PROGRAMAÇÃO C
 10) if(){} else if(){}
 11) continue => dentro de um loop, esse comando passa para a próxima iteração. Caso não queira isso, colocar i--
 12) O comando break, else if(){} e continue devem ser usados dentro de um loop nesta linguagem.
+13) Tipos de dados => Usamos double(8bytes) e float(4bytes) para representar números com casa decimal. Diferente, int(4B), short(2B) e long(8B) representam apenas números inteiros.
+14) Casting => Para que a divisão seja feita entre doubles, e por consequência, teremos casas decimais no resultado. double c = (double)a / (double)b;
+15) A função abs() pega o valor absoluto do número. Ou seja, em outras palavras, devolve sempre o valor positivo do número.
+16) Utilizo o resto (%) para limitar o intervalo de um número randômico. 
+17) time(0) => número de segundos passados desde 1970. Utilizar para alternar os números de 18*.
+18) srand()* => utilizado como passador de "semente" para a função rand alterar o seu valor
+19)
+20) 
 */
--- codificacao.c
+++ codificacao.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
-
+#include <time.h>
 int main ()
 {
     /* Bloco que configura o idioma e plota o cabeçalho
@@ -35,7 +35,7 @@ int main ()
                 printf("O número escolhido é menor que o número secreto\n");
             }   
         }*/
-
+/*
     printf("===============================================\n");
     printf("Olá! Bem-vindo ao jogo de adivinhação da Alura.\n");
     printf("===============================================\n");
@@ -68,4 +68,49 @@ int main ()
         tentativas++;
     }       
     printf("Você tentou %d vezes até acertar o número",tentativas);
+=====================================================================================================================*/
+
+    printf("===============================================\n");
+    printf("Olá! Bem-vindo ao jogo de adivinhação da Alura.\n");
+    printf("===============================================\n");
+
+    int segundos = time(0);
+    srand(segundos);
+
+    int random = rand();
+    int num_secreto = random % 100;
+    int chute;
+    int tentativas = 1;
+
+    double pontos  = 1000;
+
+    while(1)
+    {
+        printf("Tentativa %d\n",tentativas);
+        printf("Digite o chute: ");
+        scanf("%d", &chute);
+
+        //Estrutura de condições 
+        int acertou = chute == num_secreto;
+        int maior = chute > num_secreto;
+        int menor = chute < num_secreto;
+
+        if (acertou)
+        {
+            printf("Parabéns! Você acertou.\n");
+            break;
+        }
+        else if(maior){
+                printf("O número escolhido é maior que o número secreto\n");
+            }
+            else{
+                printf("O número escolhido é menor que o número secreto\n");
+            }   
+        tentativas++;
+
+        double pontosperdidos = abs(chute-num_secreto) / 2.0;
+        pontos = pontos - pontosperdidos;
+    }       
+    printf("Você tentou %d vezes até acertar o número\n",tentativas);
+    printf("Total de pontos: %.2f\n",pontos);
 }
